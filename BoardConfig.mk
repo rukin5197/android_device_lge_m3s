59bedb085818a65278e3b1a9aa2df428773a5aa0
Gannon Macdonald <gannon050197@gmail.com>
2012-11-20 19:41:01 -0600

    BoardConfig: cleanup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,59 +1,90 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-
-# Board configuration
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
 #
 
-TARGET_NO_BOOTLOADER			:= true
-TARGET_BOARD_PLATFORM			:= msm7x30
-TARGET_BOARD_PLATFORM_GPU		:= qcom-adreno200
-TARGET_CPU_ABI				:= armeabi-v7a
-TARGET_CPU_ABI2				:= armeabi
-TARGET_ARCH_VARIANT			:= armv7-a-neon
-ARCH_ARM_HAVE_TLS_REGISTER		:= true
-TARGET_PROVIDES_INIT_RC			:= true
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
 
-TARGET_GLOBAL_CFLAGS			+= -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS			+= -mfpu=neon -mfloat-abi=softfp
+USE_CAMERA_STUB := true
 
-TARGET_BOOTLOADER_BOARD_NAME		:= lge_m3s
+# Arch
+TARGET_ARCH := arm
+TARGET_ARCH := arm
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_ARCH_VARIANT := armv7-a-neon
+
+# Platform
+TARGET_BOARD_PLATFORM := msm7x30
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+# CPU
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+
+TARGET_PROVIDES_INIT_RC	:= true
+
+# Flags
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := lge_m3s
+TARGET_NO_BOOTLOADER := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom
+BOARD_KERNEL_BASE := 200000
+BOARD_FORCE_RAMDISK_ADDRESS := 1300000
+BOARD_KERNEL_PAGESIZE := 4096
 
 
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 377487360
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 947912704
+BOARD_FLASH_BLOCK_SIZE	:= 262144
 
+# Userimages
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_HAS_NO_MISC_PARTITION := true
 
-# boot.img stuff
-BOARD_KERNEL_CMDLINE			:= androidboot.hardware=qcom
-BOARD_KERNEL_BASE			:= 200000
-BOARD_FORCE_RAMDISK_ADDRESS		:= 1300000
-BOARD_KERNEL_PAGESIZE			:= 4096
+# Vold
+BOARD_VOLD_MAX_PARTITIONS := 24
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
-
-# partition info
-BOARD_BOOTIMAGE_PARTITION_SIZE		:= 8388608
-BOARD_RECOVERYIMAGE_PARTITION_SIZE	:= 8388608
-BOARD_SYSTEMIMAGE_PARTITION_SIZE	:= 377487360
-BOARD_USERDATAIMAGE_PARTITION_SIZE	:= 947912704
-BOARD_FLASH_BLOCK_SIZE			:= 262144
-
-# userimages use ext4
-TARGET_USERIMAGES_USE_EXT4		:= true
-BOARD_HAS_NO_MISC_PARTITION		:= true
-
-# vold stuff
-BOARD_VOLD_MAX_PARTITIONS		:= 24
-BOARD_VOLD_EMMC_SHARES_DEV_MAJOR	:= true
 # no clue what this does, but we have the file to support it, so lets try it out
-BOARD_USE_USB_MASS_STORAGE_SWITCH	:= true
+BOARD_USE_USB_MASS_STORAGE_SWITCH := true
 
-TARGET_NO_BOOT				:= false
-TARGET_NO_RECOVERY			:= false
+TARGET_NO_BOOT := false
+TARGET_NO_RECOVERY := false
 
 # Recovery
-TARGET_PREBUILT_KERNEL			:= device/lge/m3s/kernel
-TARGET_PREBUILT_RECOVERY_KERNEL		:= device/lge/m3s/recovery_kernel
-#TARGET_KERNEL_CONFIG			:= lge_m3s-perf_defconfig
-#TARGET_KERNEL_SOURCE			:= kernel/lge/vm696-pernel
-TARGET_RECOVERY_INITRC			:= device/lge/m3s/recovery/init.rc
-BOARD_CUSTOM_GRAPHICS			:= ../../../device/lge/m3s/recovery/graphics.c
+TARGET_PREBUILT_KERNEL := device/lge/m3s/kernel
+TARGET_PREBUILT_RECOVERY_KERNEL	:= device/lge/m3s/recovery_kernel
+#TARGET_KERNEL_CONFIG := lge_m3s-perf_defconfig
+#TARGET_KERNEL_SOURCE := kernel/lge/vm696-pernel
+TARGET_RECOVERY_INITRC := device/lge/m3s/recovery/init.rc
+BOARD_CUSTOM_GRAPHICS := ../../../device/lge/m3s/recovery/graphics.c
 
 
 # Audio, Bluetooth, camera
@@ -101,10 +132,9 @@ BOARD_USES_QCOM_LIBRPC			:= true
 
 
 
-# wifi stuff
+# Wifi related defines
 BOARD_WPA_SUPPLICANT_DRIVER		:= WEXT
-# stock wpa_supplicant is version 0.8, but cm7 only has 0.5 and 0.6
-WPA_SUPPLICANT_VERSION			:= VER_0_6_X
+WPA_SUPPLICANT_VERSION			:= VER_0_8_X
 BOARD_WLAN_DEVICE			:= wlan0
 # qualcomm's wifi.c uses these names.  we dont need them if we're using the cm7 wifi.c
 #WIFI_SDIO_IF_DRIVER_MODULE_PATH		:= "/system/lib/modules/librasdioif.ko"
@@ -120,14 +150,14 @@ WIFI_EXT_MODULE_NAME			:= "librasdioif"
 
 
 # GPS HAL from e400.  maybe ours is the same as theirs
-BOARD_USES_QCOM_LIBRPC				:= true
-BOARD_USES_QCOM_GPS				:= true
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE		:= lge_m3s
+BOARD_USES_QCOM_LIBRPC := true
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := lge_m3s
 # AMSS version to use for GPS
-BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION	:= 50000
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
 
 # modified version of the ota-zipper-upper
-TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT	:= device/lge/m3s/tools/ota_from_target_files
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/lge/m3s/tools/ota_from_target_files
 
 
 
