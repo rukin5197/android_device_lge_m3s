fd32e45ec82db41b3eb97a37e85cbcf019743a78
Gannon Macdonald <gannon050197@gmail.com>
2012-11-20 20:07:21 -0600

    BoardConfig: cleanup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,7 +55,6 @@ BOARD_KERNEL_BASE := 200000
 BOARD_FORCE_RAMDISK_ADDRESS := 1300000
 BOARD_KERNEL_PAGESIZE := 4096
 
-
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
@@ -85,10 +84,8 @@ TARGET_PREBUILT_RECOVERY_KERNEL	:= device/lge/m3s/recovery_kernel
 TARGET_RECOVERY_INITRC := device/lge/m3s/recovery/init.rc
 BOARD_CUSTOM_GRAPHICS := ../../../device/lge/m3s/recovery/graphics.c
 
-
 # Audio, Bluetooth, camera
 #BUILD_OLD_LIBCAMERA			:= true
-USE_CAMERA_STUB				:= true
 #TARGET_PROVIDES_LIBAUDIO		:= true
 #BOARD_USES_AUDIO_LEGACY			:= false
 BOARD_HAVE_BLUETOOTH			:= true
@@ -99,60 +96,45 @@ BOARD_CAMERA_USE_GETBUFFERINFO		:= true
 # RIL
 #BOARD_PROVIDES_LIBRIL			:= true
 #TARGET_PROVIDES_LIBRIL			:= device/lge/m3s/prebuilt/lib/libril.so
-
-# QCOM Stuff
+BOARD_USES_LEGACY_RIL := true
 
 # used all up in the jni
-USE_OPENGL_RENDERER			:= true
+USE_OPENGL_RENDERER := true
 # I don't see this one used for cm7
-BOARD_USES_QCOM_HARDWARE		:= true
+BOARD_USES_QCOM_HARDWARE := true
 # this is apparently used for hardware/qcom/media in cm7, but not in cm9?
-BOARD_USES_QCOM_LIBS			:= true
+BOARD_USES_QCOM_LIBS := true
 # used for some HDMI stuff in libhwcomposer
-TARGET_USES_OVERLAY			:= false
+TARGET_USES_OVERLAY := false
 # used to decide between 2 or 3 buffers in libsurfaceflinger
-TARGET_HAVE_BYPASS			:= false
+TARGET_HAVE_BYPASS := false
 # used in libcopybit makefile.  our stock one uses cd2
-TARGET_USES_C2D_COMPOSITION		:= true
+TARGET_USES_C2D_COMPOSITION := true
 # kernel should create "/dev/genlock" to use this
-TARGET_USES_GENLOCK			:= false
+TARGET_USES_GENLOCK := false
 
-TARGET_QCOM_HDMI_OUT			:= false
-BOARD_EGL_CFG				:= device/lge/m3s/egl.cfg
+TARGET_QCOM_HDMI_OUT := false
+BOARD_EGL_CFG := device/lge/m3s/egl.cfg
 
 # it looks like cm7 only uses these two when building non-msm7x30 stuff?
-TARGET_GRALLOC_USES_ASHMEM		:= true
-BOARD_USE_QCOM_PMEM			:= true
-
-BOARD_USES_QCOM_LIBRPC			:= true
-
-
-
-
-
+#TARGET_GRALLOC_USES_ASHMEM := true
 
 # Wifi related defines
-BOARD_WPA_SUPPLICANT_DRIVER		:= WEXT
-WPA_SUPPLICANT_VERSION			:= VER_0_8_X
-BOARD_WLAN_DEVICE			:= wlan0
-# qualcomm's wifi.c uses these names.  we dont need them if we're using the cm7 wifi.c
-#WIFI_SDIO_IF_DRIVER_MODULE_PATH		:= "/system/lib/modules/librasdioif.ko"
-#WIFI_SDIO_IF_DRIVER_MODULE_NAME		:= "librasdioif"
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WLAN_DEVICE := wlan0
+#WIFI_SDIO_IF_DRIVER_MODULE_PATH := "/system/lib/modules/librasdioif.ko"
+#WIFI_SDIO_IF_DRIVER_MODULE_NAME := "librasdioif"
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
+WIFI_EXT_MODULE_PATH := /system/lib/modules/librasdioif.ko
+WIFI_DRIVER_MODULE_NAME := "wlan"
+WIFI_EXT_MODULE_NAME := "librasdioif"
 
-# module paths
-WIFI_DRIVER_MODULE_PATH			:= "/system/lib/modules/wlan.ko"
-WIFI_EXT_MODULE_PATH			:= /system/lib/modules/librasdioif.ko
-# module names
-WIFI_DRIVER_MODULE_NAME			:= "wlan"
-WIFI_EXT_MODULE_NAME			:= "librasdioif"
-
-
-
-# GPS HAL from e400.  maybe ours is the same as theirs
+# Qualcomm Defines
+BOARD_USE_QCOM_PMEM := true
 BOARD_USES_QCOM_LIBRPC := true
 BOARD_USES_QCOM_GPS := true
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := lge_m3s
-# AMSS version to use for GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
 
 # modified version of the ota-zipper-upper
